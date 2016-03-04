4b77ea75813f46c2c28592a54d2fe6eaf859fc12
John Truskowski <jtruskow@searles224-13.bowdoin.edu>
2016-03-04 13:35:36 -0500

    Split view structure skeleton

---
+++ Calculator/GraphViewController.swift
@@ -0,0 +1,14 @@
+//
+//  GraphViewController.swift
+//  Calculator
+//
+//  Created by John Truskowski on 3/4/16.
+//  Copyright © 2016 John Truskowski. All rights reserved.
+//
+
+
+import UIKit
+
+class GraphViewController: UIViewController {
+
+}
